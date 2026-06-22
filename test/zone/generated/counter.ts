3c2c565201038d0d755fd5c581a3a43440a6d5fe
Lance Pollard <lp@elk.fm>
2026-06-21 19:48:34 -0700

    save

--- test/zone/generated/counter.ts
+++ test/zone/generated/counter.ts
@@ -2,8 +2,10 @@ import { signal } from '@cluesurf/make/code/zone/reactive'
 
 export function counter() {
   const [count, setCount] = signal(0)
+
   function onClick() {
     setCount(count() + 1)
   }
+
   return { count, onClick }
 }
